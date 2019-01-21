548e7f79a06db21001effeff6d43499e89002d66
Geraskina, Vera <haengbok@yandex.ru>
2019-01-21 12:35:52 +0300

    B-00000 go

--- src/main/java/com/example/d2/D2Application.java
+++ src/main/java/com/example/d2/D2Application.java
@@ -2,12 +2,21 @@ package com.example.d2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-public class D2Application {
+public class D2Application extends SpringBootServletInitializer {
 
    public static void main(String[] args) {
       SpringApplication.run(D2Application.class, args);
+   }
+
+   @Override
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(D2Application.class);
    }
 
 }
